93f74d7f4d696111eb115db5fc403919f682041c
Nhshle <Nhshle@users.noreply.github.com>
2025-06-16 20:04:19 +0300

    initial commit

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const Note = require("./models/note");
 
 const app = express();
 
-let notes = [];
-
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:  ", request.path);
@@ -14,24 +12,34 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-app.use(requestLogger);
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
 app.use(express.static("dist"));
 app.use(express.json());
+app.use(requestLogger);
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-// Get all notes
 app.get("/api/notes", (request, response) => {
   Note.find({}).then((notes) => {
     response.json(notes);
   });
 });
 
-// Get a single note by id
 app.get("/api/notes/:id", (request, response, next) => {
-  Note.findByIdAndUpdate(request.params.id)
+  Note.findById(request.params.id)
     .then((note) => {
       if (note) {
         response.json(note);
@@ -42,24 +50,8 @@ app.get("/api/notes/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-// Delete a note by id
-app.delete("/api/notes/:id", (request, response, next) => {
-  Note.findByIdAndUpdateAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
-});
-
-// Create a new note
 app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-
-  if (!body.content === undefined) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
 
   const note = new Note({
     content: body.content,
@@ -77,11 +69,7 @@ app.post("/api/notes", (request, response, next) => {
 app.put("/api/notes/:id", (request, response, next) => {
   const { content, important } = request.body;
 
-  Note.findByIdAndUpdate(
-    request.params.id,
-    { content, important },
-    { new: true, runValidators: true, context: "query" }
-  )
+  Note.findById(request.params.id)
     .then((note) => {
       if (!note) {
         return response.status(404).end();
@@ -97,27 +85,22 @@ app.put("/api/notes/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-// Unknown endpoint middleware
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
+
 app.use(unknownEndpoint);
-
-// Error handler middleware
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
-  }
-
-  next(error);
-};
 app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
